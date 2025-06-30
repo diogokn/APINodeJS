d75f429ab8de9abc0e4f5f269f149f41a642045f
Diogo Namie <41928681+diogokn@users.noreply.github.com>
2025-06-30 05:55:38 -0300

    Delete api directory

--- api/api_core/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// se está disponível o arquivo de configuração
-define('API_IS_ACTIVE', true);
-define('API_VERSION', '1.0.0');
--- api/public/v1/get_all_records/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../../api_core/config.php';
-require_once __DIR__ . '/../../../api_core/response.php';
-
-$data  = require_once __DIR__ . '/../../../api_core/data.php';
-
-echo Response::json(200, 'success', ['total_records' => count($data)]);
